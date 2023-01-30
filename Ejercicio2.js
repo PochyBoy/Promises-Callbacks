b9c65ab1a644c6e366c02d231e9f076f87899824
Paul Vargas Portugal <videoblogpaul@gmail.com>
2023-01-30 12:55:25 +0100

    Terminado el ejercicio 1 y a medias el ejercio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,41 @@
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+/* Ejercicio 1*/
+
+let getEmpoyee = (ide)=> { 
+    return new Promise((resolve,reject)=>{
+      let prueba = employees.find(item => item.id == ide)
+     if(prueba != undefined){
+       resolve(`El nombre del empleado es ${prueba.name}`)
+     }else{
+          reject(new Error('No existe este empleado'))
+      } 
+    })   
+  }
+  getEmpoyee(5).then(res => {
+    console.log(res)
+  })
+  .catch(error => console.log(error.message))
+
+/* Ejercicio 2*/
+
